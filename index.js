dc24fc0626d3bbba6fdf6cb732b60b40fa243a5b
Zongmin Lei <leizongmin@gmail.com>
2016-10-11 13:12:54 +0800

    增加注释

--- index.js
+++ index.js
@@ -232,7 +232,9 @@ module.exports = {
     'prefer-const': 'warn',
 
     // ------------------------ Pomise 强制的风格 --------------------------------
+    // 调用 Pomise 函数时，必须有 .catch(callback) 或者用 return 返回当前 Promise 对象
     'promise/catch-or-return': 'error',
+    // new Promise() 时，函数的参数名只能为标准的 resolve, reject
     'promise/param-names': 'error',
 
   },
